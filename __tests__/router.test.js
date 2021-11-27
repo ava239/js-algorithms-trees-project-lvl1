a653bfff400f029dc53f7f626aabcb2f2b2affcc
Ava <ava239@yandex.ru>
2021-11-27 23:08:07 +0300

    static routing

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -1,15 +1,5 @@
-import { fileURLToPath } from 'url';
-import fs from 'fs';
-import path from 'path';
 import { test, expect } from '@jest/globals';
 import makeRouter from '../src/router.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const read = (filePath) => fs.readFileSync(filePath, 'utf-8').trim();
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-
-const getFixture = (id) => read(getFixturePath(`${id}`)); // eslint-disable-line
 
 test('basic', () => {
   const routes = [
@@ -29,5 +19,6 @@ test('basic', () => {
   const handler = router.serve(path);
   expect(handler()).toEqual('courses!');
 
-  expect(router.serve('/no_such_way')).toThrow(Error);
+  const errorHandler = () => router.serve('/no_such_way');
+  expect(errorHandler).toThrow(Error);
 });
--- src/router.js
+++ src/router.js
@@ -1,5 +1,22 @@
 // @ts-check
 
-const makeRouter = () => {};
+const makeRouter = (routeList) => {
+  const router = {
+    routes: {},
+    serve: (path) => {
+      const { routes } = router;
+      if (!Object.keys(routes).includes(path)) {
+        throw Error('no such route');
+      }
+      return routes[path];
+    },
+  };
+
+  routeList.forEach(({ path, handler }) => {
+    router.routes[path] = handler;
+  });
+
+  return router;
+};
 
 export default makeRouter;
