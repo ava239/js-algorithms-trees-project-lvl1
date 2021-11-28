c009bdfaedbc90d28cb02c86c66834203938630b
Ava <ava239@yandex.ru>
2021-11-29 00:22:04 +0300

    remove routeTree from returned object

--- src/router.js
+++ src/router.js
@@ -2,19 +2,17 @@
 import PrefixTree from './prefixTree';
 
 const makeRouter = (routeList) => {
-  const router = {
-    routeTree: new PrefixTree(routeList),
+  const routeTree = new PrefixTree(routeList);
+  return {
     serve: (path) => {
       const pathParts = path.split('/').slice(1);
-      const routeData = router.routeTree.find(pathParts);
+      const routeData = routeTree.find(pathParts);
       if (routeData) {
         return routeData;
       }
       throw Error('no such route');
     },
   };
-
-  return router;
 };
 
 export default makeRouter;
