7128648de6062480b9ff3ae58e8b110476e09622
Ava <ava239@yandex.ru>
2021-12-11 23:08:16 +0300

    logs

--- src/router.js
+++ src/router.js
@@ -3,8 +3,10 @@ import PrefixTree from './prefixTree';
 
 const makeRouter = (routeList) => {
   const routeTree = new PrefixTree(routeList);
+  console.dir(routeTree, { depth: null });
   return {
     serve: (request) => {
+      console.log(request);
       const path = typeof request === 'string' ? request : request.path;
       const method = request.method ?? 'GET';
       const pathParts = path.split('/').slice(1);
